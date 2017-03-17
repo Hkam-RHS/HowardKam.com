d5df8c1fa4e24ce529fc0d2b3757a6086db824a6
Howard Kam <hkam@roosevelths.k12.hi.us>
2017-03-16 23:22:23 -1000

    Rewrite everything

--- js/init.js
+++ js/init.js
@@ -2,17 +2,7 @@
   $(function(){
 
     $('.button-collapse').sideNav();
+    $('.parallax').parallax();
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
-
-
-    $(document).ready(function(){
-      $('.slider').slider({full_width: true});
-    });
-    
-    $(document).ready(function(){
-      $('.parallax').parallax();
-    });
-        
-        
